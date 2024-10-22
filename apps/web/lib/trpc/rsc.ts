a6d4a9cb47a49ace3c405c26bb1daab4ae625c47
Eelco Wiersma <eelco@appulse.nl>
2024-10-22 10:13:16 +0100

    feat: fix header warnings and upgrade charts

--- apps/web/lib/trpc/rsc.ts
+++ apps/web/lib/trpc/rsc.ts
@@ -1,7 +1,6 @@
 import { cache } from 'react'
 
 import { createHydrationHelpers } from '@trpc/react-query/rsc'
-import { headers as getHeaders } from 'next/headers'
 
 import { createCaller, createTRPCContext } from '@acme/api'
 import type { AppRouter } from '@acme/api/types'
@@ -16,17 +15,15 @@ import { createAdapters } from './adapters'
  * handling a tRPC call from a React Server Component.
  */
 const createContext = cache(async () => {
-  const headers = new Headers(await getHeaders())
-
-  // headers.set('x-trpc-source', 'rsc')
-  // console.log(headers)
   const adapters = createAdapters()
 
   const session = await getSession()
 
   return createTRPCContext({
     session,
-    headers,
+    headers: new Headers({
+      'x-trpc-source': 'rsc',
+    }),
     adapters,
   })
 })
