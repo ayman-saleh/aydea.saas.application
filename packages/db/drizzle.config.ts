204bed8522822e9ba21a238ddbe6d09bfe61e95b
Eelco Wiersma <eelco@appulse.nl>
2025-01-30 11:24:30 +0000

    fix: resolve auth schema with turbo

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,5 +1,6 @@
 import * as dotenv from 'dotenv'
 import { defineConfig } from 'drizzle-kit'
+import path from 'node:path'
 
 dotenv.config({ path: ['../../.env', '../../.env.local'] })
 
@@ -12,7 +13,12 @@ const connectionString = process.env.DATABASE_URL
 export default defineConfig({
   dialect: 'postgresql',
   strict: true,
-  schema: ['./src/**/*.sql.ts', '../better-auth/src/auth.sql.ts'],
+  schema: [
+    './src/**/*.sql.ts',
+    // We need to use path.resolve here otherwise
+    // it's not resolved correctly running drizzle through turbo.
+    path.resolve(__dirname, '../better-auth/src/auth.sql.ts'),
+  ],
   out: './drizzle',
   verbose: true,
   dbCredentials: { url: connectionString },
