96229d5e395d07f92ebc974dc6ebf5b091e59277
Eelco Wiersma <eelco@appulse.nl>
2025-02-17 11:07:38 +0000

    feat: export router input output types from trpc lib

--- apps/web/lib/trpc/react.ts
+++ apps/web/lib/trpc/react.ts
@@ -2,6 +2,8 @@ import { TRPCClientError, createTRPCReact } from '@trpc/react-query'
 
 import type { AppRouter } from '@acme/api/types'
 
+export type { RouterInputs, RouterOutputs } from '@acme/api/types'
+
 export const api = createTRPCReact<AppRouter>()
 
 export function isTRPCClientError(
